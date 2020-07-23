b01205bd93eca1879dd90fff0e95854b24d6a774
幸成 <573105494@qq.com>
2020-07-23 19:13:28 +0800

    配置静态文件

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 const app = express();
 
 // DB config
@@ -14,6 +15,9 @@ const profiles = require('./routes/api/profiles');
 // 使用body-parser中间件
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// 设置静态文件
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 使用routes
 app.use("/api/users", users);
